e22fa52db43bfb1fcce82c1447fe69213fadd79f
Behnoosh Mahmoodi <behnoosh.mahmoodi@gmail.com>
2021-05-06 15:53:24 -0400

    creating seeds

---
+++ sql/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department(id, name)
+VALUES(2410, "Developer");
+
+INSERT INTO department(id, name)
+VALUES(2411, "Jr Developer");
+
+INSERT INTO department(id, name)
+VALUES(2412, "Marketing Manager");
+
+INSERT INTO department(id, name)
+VALUES(2413, "Manager");
+
+INSERT INTO department(id, name)
+VALUES(2414, "HR Manager");
+
+INSERT INTO department(id, name)
+VALUES(2415, "Engineer");
+
+INSERT INTO department(id, name)
+VALUES(2416, "salePerson");
+
