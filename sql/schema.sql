99b4478b6d937b73db4310f89a6d8c2faace95df
Behnoosh Mahmoodi <behnoosh.mahmoodi@gmail.com>
2021-05-06 15:10:18 -0400

    adding commend

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,4 +7,22 @@ CREATE TABLE department(
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE TABLE role(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary INT(10) NOT NULL,
+departmentId INT(10),  -- it reference to depertment table
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT(10), -- INT to hold reference to role employee has
+manager_id INT(10), -- hold reference to another employee 
+					-- that manages employee being Created. 
+                    -- This field may be null if the employee has no manager
+PRIMARY KEY(id)
+);
 
