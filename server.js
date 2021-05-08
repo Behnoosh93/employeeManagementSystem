ecc7ca07e3e9b769705163830ab93e527a97d78d
Behnoosh Mahmoodi <behnoosh.mahmoodi@gmail.com>
2021-05-08 11:13:31 -0400

    deleting doubel required package

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const connection = require("./lib/login");
 const inquirer = require("inquirer");
 const figlet = require("figlet");
 
-
-
-// Required Packages
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const figlet = require("figlet");
-
 figlet("employee managment system", function (err, data) {
 	if (err) {
 		console.log("Something is wrong...");
